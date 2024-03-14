9ed551f119d7c62ed159cf54ba08585d65432370
Ledres1985 <94397954+Ledres1985@users.noreply.github.com>
2024-03-14 10:35:20 -0300

    Update conexao.php

--- crud_pessoas/conexao.php
+++ crud_pessoas/conexao.php
@@ -1,14 +1,4 @@
 <?php
- /*try{
-  $conn = new PDO('sqlite3:cadastro');
- 
-  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-  echo "Conexão realizada com sucesso.";  
- } catch (PDOException $e){
-    echo "Falha de conexão ". $e->getMessage(); 
- }
-*/
 
  $conn = new sqlite3("cadastro.db");
  
